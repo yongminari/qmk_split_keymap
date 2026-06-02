3356caf232a24f2435d27499cdd9afb78d0845d0
yongminari <easyid21c@gmail.com>
2026-06-03 00:31:11 +0900

    change led

--- keymaps/yongminari/config.h
+++ keymaps/yongminari/config.h
@@ -13,3 +13,17 @@
 /* 분할 키보드 통신 최적화 */
 #define SPLIT_USB_DETECT
 #define MASTER_LEFT
+
+/* RGB Matrix 설정 */
+#ifdef RGB_MATRIX_ENABLE
+#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE // 누른 키 주변까지 불빛이 퍼지는 효과
+#    define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+#    define RGB_MATRIX_DEFAULT_HUE 0
+#    define RGB_MATRIX_DEFAULT_SAT 0   // 0이면 흰색에 가까움, 255면 원색
+#    define RGB_MATRIX_DEFAULT_VAL 150 // 기본 밝기
+#    undef RGB_MATRIX_MAXIMUM_BRIGHTNESS
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // 최대 밝기 제한
+#    define RGB_MATRIX_LED_FLUSH_LIMIT 16
+#    define RGB_MATRIX_KEYPRESSES // 키 누름에 반응하도록 설정
+#    define RGB_MATRIX_TIMEOUT 30000 // 30초 후 LED 끄기
+#endif
